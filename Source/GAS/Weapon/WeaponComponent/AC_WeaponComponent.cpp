6ad6cded80e4acd1f96e79fb1c2920a89e80ec11
Kurosagi <nani71137@hotmail.com>
2026-09-15 08:25:10 +0900

    Create/C++InWeaponComponent

---
+++ Source/GAS/Weapon/WeaponComponent/AC_WeaponComponent.cpp
@@ -0,0 +1,277 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AC_WeaponComponent.h"
+#include "GAS/GameplayAbilitySystem/Characters/NexusCharacterBase.h"
+#include "GAS/Weapon/WeaponBase/WeaponBase.h"
+#include "Net/UnrealNetwork.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Animation/AnimInstance.h"
+
+
+UAC_WeaponComponent::UAC_WeaponComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+	
+	// コンポーネントのレプリケーションを有効にする
+	SetIsReplicatedByDefault(true);
+
+
+}
+
+void UAC_WeaponComponent::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	
+	//EquippedWeapon変数をレプリケーション対象に登録。
+	DOREPLIFETIME(UAC_WeaponComponent,EquippedWeapon);
+	//PreviouslyEquippedWeapon変数をレプリケーション対象に登録。
+	DOREPLIFETIME(UAC_WeaponComponent,PreviouslyEquippedWeapon);
+	
+}
+
+void UAC_WeaponComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	// Ownerを取得し、NexusCharacterBaseにキャストして保存
+	OwningCharacter = Cast<ANexusCharacterBase>(GetOwner());
+	
+	// Authority(サーバー)でのみ実行
+	if (OwningCharacter && OwningCharacter -> HasAuthority())
+	{
+		// StartingWeapons配列をループして武器を付与
+		for (TSubclassOf<AWeaponBase> WeaponClass : StartingWeapons)
+		{
+			if (WeaponClass)
+			{
+				//GiveWeapon関数を実行
+				GiveWeapon(WeaponClass);
+			}
+		}
+	}
+}
+
+void UAC_WeaponComponent::OnRep_EquippedWeapon()
+{
+	// サーバーの武器の見た目をクライアントに反映させる処理
+	if (IsValid(EquippedWeapon))
+	{
+		SetEquippedWeaponProperties();
+	}
+	else
+	{
+		SetUnarmedWeaponConfig();
+	}
+}
+
+void UAC_WeaponComponent::GiveWeapon(TSubclassOf<AWeaponBase> WeaponClass)
+{
+	
+	UWorld* World = GetWorld();
+	if (!OwningCharacter ||	!WeaponClass || !World)
+	{
+		return;
+	}
+	
+	// Spawn Actor 用のパラメータ設定
+	FActorSpawnParameters SpawnParameters;
+	SpawnParameters.Instigator = OwningCharacter;
+	SpawnParameters.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+	
+	//スポーンアクター
+	AWeaponBase* SpawnWeapon = World->SpawnActor<AWeaponBase>(
+		WeaponClass,FVector::ZeroVector,FRotator::ZeroRotator,SpawnParameters);
+	
+	if (SpawnWeapon)
+	{
+		// StowedWeapons配列にユニーク追加
+		StowedWeapons.AddUnique(SpawnWeapon);
+		
+		// 武器のコンフィグからアタッチ用のソケット名を取得
+		FName SocketName = SpawnWeapon->WeaponConfig.StowedSocketName;
+		
+		// OwningCharacterのメッシュを取得してアタッチ
+		if (USkeletalMeshComponent* CharacterMesh = OwningCharacter->GetMesh())
+		{
+			// Location/Rotation/Scale は Keep Relative (相対位置を維持) を指定
+			SpawnWeapon->AttachToComponent(CharacterMesh,FAttachmentTransformRules::KeepRelativeTransform,SocketName);
+		}
+	}
+}
+
+void UAC_WeaponComponent::EquipWeapon(TSubclassOf<AWeaponBase> WeaponClass)
+{
+	// サーバーのみ実行 (Switch Has Authority)
+	if (!GetOwner()->HasAuthority())
+	{
+		return;
+	}
+	
+	//OwningCharacterの有効チェック
+	if (!IsValid(OwningCharacter))
+	{
+		return;
+	}
+	
+	// 現在の武器クラスをチェックし、同じなら終了、違うなら外す
+	if (IsValid(EquippedWeapon))
+	{
+		if (EquippedWeapon->GetClass() == WeaponClass)
+		{
+			// 等しい場合は武器をすでに持っていて、武器をしまうことを示唆している。
+			UnequipWeapon();
+			
+			return;
+		}
+		else
+		{
+			// 等しくない場合はUnequipを実行
+			UnequipWeapon();
+		}
+	}
+	
+	//取り出す武器が背中にある武器と等しいか調べる。
+	AWeaponBase* StowedWeapon = GetStowedWeaponByClass(WeaponClass);
+	
+	if (IsValid(StowedWeapon))
+	{
+		// 武器をセットして通知（レプリケート)
+		EquippedWeapon = StowedWeapon;
+		
+		// 装備武器のプロパティの設定関数。
+		SetEquippedWeaponProperties();
+	}
+}
+
+void UAC_WeaponComponent::UnequipWeapon()
+{
+	// EquippedWeaponがValidならPreviouslyEquippedWeaponに保存。
+	if (IsValid(EquippedWeapon))
+	{
+		PreviouslyEquippedWeapon = EquippedWeapon;
+	}
+	// EquippedWeaponを空にする。
+	EquippedWeapon = nullptr;
+	
+	// 解除武器のプロパティの設定関数を実行。
+	SetUnarmedWeaponConfig();
+}
+
+AWeaponBase* UAC_WeaponComponent::GetStowedWeaponByClass(TSubclassOf<AWeaponBase> WeaponClass)
+{
+	// 配列をループしてクラスが一致するものを探す
+	for (AWeaponBase* Weapon : StowedWeapons)
+	{
+		if (IsValid(Weapon) && Weapon->GetClass() == WeaponClass)
+		{
+			// 一致したら値を返す
+			return Weapon;
+		}
+	}
+	// 見つからなかった場合はnullptrを返す
+	return nullptr;
+}
+
+void UAC_WeaponComponent::SetEquippedWeaponProperties()
+{
+	// EquippedWeaponとOwningCharacterのValidチェック
+	if (!IsValid(EquippedWeapon) || !IsValid(OwningCharacter))
+	{
+		return;
+	}
+	
+	// 武器の親アクターから継承した構造体を取り出す (Break SWeaponConfig)
+	FSWeaponConfig WeaponConfig=EquippedWeapon->WeaponConfig;
+	
+	// メッシュの取得
+	USkeletalMeshComponent* CharacterMesh = OwningCharacter->GetMesh();
+	if (!IsValid(CharacterMesh))
+	{
+		return;
+	}
+	
+	// AnimInstanceClassを設定
+	if (WeaponConfig.AnimClass)
+	{
+		CharacterMesh->SetAnimInstanceClass(WeaponConfig.AnimClass);
+	}
+	
+	// 移動速度/カメラを設定 (Break SMovementProperties)
+	UCharacterMovementComponent* MovementComp = OwningCharacter->GetCharacterMovement();
+	if (IsValid(MovementComp))
+	{
+		FSMovementProperties MoveProps = WeaponConfig.MovementProperties;
+		
+		MovementComp->MaxWalkSpeed = MoveProps.MaxWalkSpeed;
+		MovementComp->bOrientRotationToMovement = MoveProps.OrientRotationToMovement;
+		MovementComp->bUseControllerDesiredRotation = MoveProps.UseControllerDesiredRotation;
+	}
+	
+	//武器を変更したことをコール (イベントディスパッチャー)
+	OnWeaponChanged.Broadcast(EquippedWeapon);
+	
+	//BPでPlayMontageは任せる
+	if (IsValid(EquippedWeapon))
+	{
+		PlayEquipMontage(EquippedWeapon->WeaponConfig);
+	}
+}
+
+void UAC_WeaponComponent::SetUnarmedWeaponConfig()
+{
+	if (!IsValid(OwningCharacter))
+	{
+		return;
+	}
+	
+	USkeletalMeshComponent* CharacterMesh = OwningCharacter->GetMesh();
+	
+	if (!IsValid(CharacterMesh))
+	{
+		return;
+	}
+	
+	// AnimInstanceClassを設定
+	if (UnarmedWeaponConfig.AnimClass)
+	{
+		CharacterMesh->SetAnimInstanceClass(UnarmedWeaponConfig.AnimClass);
+	}
+	
+	//BPでPlayMontageは任せる
+	if (IsValid(PreviouslyEquippedWeapon))
+	{
+		PlayUnequipMontage(PreviouslyEquippedWeapon->WeaponConfig);
+	}
+}
+
+void UAC_WeaponComponent::SetUnarmedProperties()
+{
+	// UnarmedWeaponConfig変数から構造体を取り出す (Break SWeaponConfig)
+	FSWeaponConfig WeaponConfig = UnarmedWeaponConfig;
+	
+	UCharacterMovementComponent* MovementComp = OwningCharacter->GetCharacterMovement();
+	if (IsValid(MovementComp))
+	{
+		FSMovementProperties MoveProps = WeaponConfig.MovementProperties;
+		
+		MovementComp->MaxWalkSpeed = MoveProps.MaxWalkSpeed;
+		MovementComp->bOrientRotationToMovement = MoveProps.OrientRotationToMovement;
+		MovementComp->bUseControllerDesiredRotation = MoveProps.UseControllerDesiredRotation;
+	}
+	
+	//武器を変更したことをコール (イベントディスパッチャー)
+	OnWeaponChanged.Broadcast(nullptr);
+	
+	IsWieldingWeapon = false;
+}
+
+
+void UAC_WeaponComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                        FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
+
+
+
---
+++ Source/GAS/Weapon/WeaponComponent/AC_WeaponComponent.h
@@ -0,0 +1,106 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "GAS/GamePlayAbilitySystem/Characters/NexusCharacterBase.h"
+#include "GAS/Weapon/WeaponBase/WeaponBase.h"
+#include "AC_WeaponComponent.generated.h"
+
+// イベントディスパッチャー用のデリゲート宣言
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnWeaponChanged,AWeaponBase*,EquippedWeapon);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnWeaponUnequipped);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnWeaponEquipped);
+
+UCLASS(Blueprintable,ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class GAS_API UAC_WeaponComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+
+	UAC_WeaponComponent();
+	
+	// イベントディスパッチャー
+	UPROPERTY(BlueprintAssignable,BlueprintCallable,Category= "Weapon Events");
+	FOnWeaponChanged OnWeaponChanged;
+	
+	UPROPERTY(BlueprintAssignable,BlueprintCallable,Category= "Weapon Events");
+	FOnWeaponUnequipped OnWeaponUnequipped;
+	
+	UPROPERTY(BlueprintAssignable,BlueprintCallable,Category= "Weapon Events");
+	FOnWeaponEquipped OnWeaponEquipped;
+
+protected:
+
+	virtual void BeginPlay() override;
+	
+	// EquippedWeaponがレプリケートされた時に呼ばれる関数
+	UFUNCTION()
+	void OnRep_EquippedWeapon();
+
+	// 武器を付与する関数
+	UFUNCTION(BlueprintCallable, Category= "Weapon")
+	void GiveWeapon(TSubclassOf<AWeaponBase> WeaponClass);
+	
+	// 武器を装備する関数
+	UFUNCTION(BlueprintCallable, Category= "Weapon")
+	void EquipWeapon (TSubclassOf<AWeaponBase> WeaponClass);
+	
+	// 武器を解除する関数
+	UFUNCTION(BlueprintCallable, Category= "Weapon")
+	void UnequipWeapon();
+	
+	//取り出す武器が背中にある武器と等しいか調べる関数。
+	UFUNCTION(BlueprintCallable, Category= "Weapon")
+	AWeaponBase* GetStowedWeaponByClass(TSubclassOf<AWeaponBase> WeaponClass);
+	
+	// 装備武器のプロパティの設定関数。
+	UFUNCTION(BlueprintCallable, Category= "Weapon")
+	void SetEquippedWeaponProperties();
+	
+	// 解除武器のプロパティの設定関数。
+	UFUNCTION(BlueprintCallable, Category= "Weapon")
+	void SetUnarmedWeaponConfig();
+	
+	//BPでEquipMontageを再生する関数
+	UFUNCTION(BlueprintImplementableEvent,Category="Weapon")
+	void PlayEquipMontage(FSWeaponConfig WeaponConfig);
+	
+	//BPでUnequipMontageを再生する関数
+	UFUNCTION(BlueprintImplementableEvent,Category="Weapon")
+	void PlayUnequipMontage(FSWeaponConfig WeaponConfig);
+	
+	//移動速度/カメラ設定をUnarmedに戻す関数
+	UFUNCTION(BlueprintCallable,Category= "Weapon")
+	void SetUnarmedProperties();
+	
+protected:
+	UPROPERTY(BlueprintReadOnly,Category= "Weapon")
+	ANexusCharacterBase* OwningCharacter;
+	
+	UPROPERTY(EditAnywhere,BlueprintReadOnly,Category= "Weapon")
+	TArray<TSubclassOf<AWeaponBase>> StartingWeapons;
+	
+	UPROPERTY(BlueprintReadOnly,Category= "Weapon")
+	TArray<AWeaponBase*> StowedWeapons;
+	
+	UPROPERTY(BlueprintReadWrite,Category= "Weapon")
+	bool IsWieldingWeapon;
+	
+	UPROPERTY(BlueprintReadWrite,ReplicatedUsing = OnRep_EquippedWeapon,Category= "Weapon")
+	AWeaponBase* EquippedWeapon;
+	
+	UPROPERTY(BlueprintReadOnly,Replicated,Category= "Weapon")
+	AWeaponBase* PreviouslyEquippedWeapon;
+	
+	UPROPERTY(BlueprintReadOnly,Category="Weapon")
+	FSWeaponConfig UnarmedWeaponConfig;
+
+	
+public:
+
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+};
